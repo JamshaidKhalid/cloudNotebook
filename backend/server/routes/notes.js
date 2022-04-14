487b44cbe0c1e2f74313c70d6fc524c6ca559ffe
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-14 16:24:25 +0500

    note updated end point

--- backend/server/routes/notes.js
+++ backend/server/routes/notes.js
@@ -18,7 +18,7 @@ router.get('/all-notes', fetchUser, async (req, res) => {
 });
 
 
-//ROUTE 2: creating a new user of the USER WHO IS LOGGED IN    localhost/api/notes/new-note,    AUTHENTICATION/LOGIN REQUIRED
+//ROUTE 2: creating a new note of the USER WHO IS LOGGED IN    localhost/api/notes/new-note,    AUTHENTICATION/LOGIN REQUIRED
 router.post('/new-note', fetchUser, [
     body('title').not().isEmpty().withMessage('Title is required').isLength({ min: 3 }),
     body('description').not().isEmpty().withMessage('Description is required, MIN: 5').isLength({ min: 5 })
@@ -36,14 +36,63 @@ router.post('/new-note', fetchUser, [
             return res.status(422).json({ errors: errors.array() });
         }
 
-        const { title, description, tag } = req.body;
+        
 
         const note = await new Notes({
-            title, description, tag, user: req.user.id
+            title: req.body.title, 
+            description: req.body.description, 
+            tag: req.body.tag, 
+            user: req.user.id       //this is coming from middleware which gives userdetails using authtoken
         });
 
+        res.send('Note saved successfully');
+        await note.save().catch(err => console.log(err));
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Some error has occured");
+    }
+});
 
-        note.save().catch(err => console.log(err));
+
+
+
+//ROUTE 3: upadating an existing note of the USER WHO IS LOGGED IN    localhost/api/notes/update-note,    AUTHENTICATION/LOGIN REQUIRED
+router.post('/new-note:id', fetchUser, async (req, res) => {
+
+    try {
+        const {title, description, tag} = req.body;
+        const note = await Notes.findById(req.params.id);
+
+
+        /** 
+         The following lines mean that if title is give in body to be updated then it will update the title using the instance note.title
+         similarly for others
+         */
+        if(title) {note.title = title;}
+        if(description) {note.description = description;}
+        if(tag) {note.tag = tag;}
+
+
+
+        //if the the request note doens't exist then it will send an error
+        if(!note) {
+            return res.status(404).send('Note not found');
+        }
+
+        //if the user who is logged in is not the owner of the note then it will send an error
+        //fetchUser will give the userID of the logged in user and the userID of the note is stored in the userId field of the note
+        //we will compare these both
+
+        if(note.userId != req.user.id) {
+            return res.status(401).send('Unauthorized');
+        }
+
+
+        //if the user who is logged in is the owner of the note then it will update the note
+        note.findByIdAndUpdate(req.params.id, note, {new: true});       
+
+
+        res.send('Note updated successfully');
     } catch (error) {
         console.log(error);
         res.status(500).send("Some error has occured");
