ac921dcd3fbc9e4c34017c1903b166854315f050
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-14 17:30:14 +0500

    delete note done

--- backend/server/routes/notes.js
+++ backend/server/routes/notes.js
@@ -102,4 +102,34 @@ router.put('/update-note/:id', fetchUser, async (req, res) => {
 });
 
 
+//ROUTE 4: deleting an existing note of the USER WHO IS LOGGED IN    localhost/api/notes/delete-note,    AUTHENTICATION/LOGIN REQUIRED
+router.delete('/delete-note/:id', fetchUser, async (req, res) => {
+
+    try {
+        const note = await Notes.findById(req.params.id);
+
+
+        //if the the request note doens't exist then it will send an error
+        if(!note) {
+            return res.status(404).send('Note not found');
+        }
+
+        //if the user who is logged in is not the owner of the note then it will send an error
+        //fetchUser will give the userID of the logged in user and the userID of the note is stored in the userId field of the note
+        //we will compare these both
+
+        if(note.user.toString() !== req.user.id) {
+            return res.status(401).send('Unauthorized');
+        }
+
+
+        //if the user who is logged in is the owner of the note then it will update the note
+        await Notes.findByIdAndDelete(req.params.id); 
+        res.send('Note deleted successfully');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Some error has occured");
+    }
+});
+
 module.exports = router;
