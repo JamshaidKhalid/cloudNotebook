3b2c437ddf99f917acda404685408848329abd82
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-05-18 22:48:13 +0500

    Basic route components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my cloudNotebook</h1>
-    </div>
+    <Navbar/>
+
   );
 }
 
